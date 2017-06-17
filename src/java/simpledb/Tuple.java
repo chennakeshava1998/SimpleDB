1b16d66635a34007ec1ed95b22107a2a49bad01f
RosaGeorge31 <rosageorge97@gmail.com>
2017-06-18 01:05:12 +0400

    Update Tuple.java

--- src/java/simpledb/Tuple.java
+++ src/java/simpledb/Tuple.java
@@ -3,6 +3,9 @@ package simpledb;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
+
+import simpledb.TupleDesc.TDItem;
 
 /**
  * Tuple maintains information about the contents of a tuple. Tuples have a
@@ -13,6 +16,11 @@ public class Tuple implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    //create a new private variable
+    private TupleDesc tupDesc;
+    private Field[] Obj;    
+    private RecordId record_Id;
+    
     /**
      * Create a new tuple with the specified schema (type).
      * 
@@ -22,6 +30,12 @@ public class Tuple implements Serializable {
      */
     public Tuple(TupleDesc td) {
         // some code goes here
+    	  	if(tupDesc.numFields() < 1)
+    		throw new IllegalArgumentException(td.toString());
+    	
+    	tupDesc = td;
+    	Obj = new Field[td.numFields()];
+    	record_Id = null;
     }
 
     /**
@@ -29,7 +43,8 @@ public class Tuple implements Serializable {
      */
     public TupleDesc getTupleDesc() {
         // some code goes here
-        return null;
+    	
+        return tupDesc;
     }
 
     /**
@@ -37,8 +52,7 @@ public class Tuple implements Serializable {
      *         be null.
      */
     public RecordId getRecordId() {
-        // some code goes here
-        return null;
+        return record_Id;
     }
 
     /**
@@ -48,7 +62,7 @@ public class Tuple implements Serializable {
      *            the new RecordId for this tuple.
      */
     public void setRecordId(RecordId rid) {
-        // some code goes here
+    	record_Id = rid;
     }
 
     /**
@@ -61,6 +75,13 @@ public class Tuple implements Serializable {
      */
     public void setField(int i, Field f) {
         // some code goes here
+    	
+    	if(i<0 || i>=Obj.length)
+    	throw new IllegalArgumentException("Invalid input, either out of bound or negative");
+    	if(f.getType().equals(Obj[i].getType()))
+    		Obj[i] = f;
+    	else
+    	throw new IllegalArgumentException("Input Type Mismatch");
     }
 
     /**
@@ -71,7 +92,10 @@ public class Tuple implements Serializable {
      */
     public Field getField(int i) {
         // some code goes here
-        return null;
+    	if(i<0 || i >= Obj.length)
+    		throw new IllegalArgumentException("Invalid input, either out of bound or negative.");
+    	 else
+        return Obj[i];
     }
 
     /**
@@ -83,17 +107,40 @@ public class Tuple implements Serializable {
      * where \t is any whitespace, except newline, and \n is a newline
      */
     public String toString() {
-        // some code goes here
-        throw new UnsupportedOperationException("Implement this");
-    }
+    	
+    	String result = "";
+    	for(int i=0; i<Obj.length; i++)
+    	{
+    		String temp = "";
+    		if(i == Obj.length-1)
+    			temp = Obj[i] + "\n";
+    		else
+    			temp = Obj[i] + "\t";
+    		
+    		result+=temp;
+    	}
+    	
+    	return result;
+    	
+         }
     
     /**
      * @return
      *        An iterator which iterates over all the fields of this tuple
      * */
     public Iterator<Field> fields()
+    {   	
+    	List<Field> fileList = Arrays.asList(Obj);//converting to list to make it easier to use iterator
+    	Iterator<Field> itr = fileList.iterator();
+        return itr;
+    }
+    
+    /**
+     * reset the TupleDesc of this tuple
+     * */
+    public void resetTupleDesc(TupleDesc td)
     {
         // some code goes here
-        return null;
+    	tupDesc = td;
     }
 }
